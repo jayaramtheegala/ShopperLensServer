75069b8bd0ea542bd99a95c8d4a33b71f6f11297
Sairam Arunachalam <sair.aruna@gmail.com>
2015-05-23 18:37:00 -0700

    Adding PUT requests

--- app/routes.js
+++ app/routes.js
@@ -14,6 +14,21 @@ router.route('/product')
   /* Handle HTTP POST requests. */
   .post(function(req,res){
     res.json({product:req.body.product_name});
-  });
+  })
 
+  .put(function(req,res){
+    var prodReview = new review();
+    prodReview.productName = req.body.name;
+    prodReview.review = req.body.review;
+    prodReview.name = req.body.user_name;
+    prodReview.rating = req.body.rating;
+
+
+    prodReview.save(function(err){
+      if(err){
+        res.json({Error:'Error while creating review'});
+      }
+      res.json({success:'Review created.'});
+    });
+  })
 module.exports = router;
